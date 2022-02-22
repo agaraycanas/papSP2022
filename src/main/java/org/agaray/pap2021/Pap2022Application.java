64ed5da47b83f08350b23b038795a80c96fb1419
Alberto Garay <alberto@loquesea>
2022-02-22 13:29:23 +0100

    Último commit

--- src/main/java/org/agaray/pap2021/Pap2022Application.java
+++ src/main/java/org/agaray/pap2021/Pap2022Application.java
@@ -1,5 +1,6 @@
 package org.agaray.pap2021;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Pap2022Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pap2022Application.class, args);
 	}
--- src/main/java/org/agaray/pap2021/controller/HomeController.java
+++ src/main/java/org/agaray/pap2021/controller/HomeController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+	
 
 	@Autowired
 	private PersonaRepository personaRepository;
