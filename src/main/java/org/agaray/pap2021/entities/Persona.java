b20c7d9aa9ccc949d7de711c348a58ad011bc39b
Alberto Garay(dpto.Info) <garay_alberto@hotmail.com>
2022-03-17 08:59:51 +0100

    commit local en el dpto

--- src/main/java/org/agaray/pap2021/entities/Persona.java
+++ src/main/java/org/agaray/pap2021/entities/Persona.java
@@ -5,10 +5,8 @@ import java.time.Period;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
