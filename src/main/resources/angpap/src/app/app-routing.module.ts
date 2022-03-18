7bdce3332b06665f7bc3d754863c3afd092f4bb5
Alberto Garay(dpto.Info) <garay_alberto@hotmail.com>
2022-03-18 12:32:17 +0100

    Jugando con rutas. No funciona

--- src/main/resources/angpap/src/app/app-routing.module.ts
+++ src/main/resources/angpap/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './pais/index/index.component';
+import { PaisRoutingModule } from './pais/pais-routing.module';
+import { PaisModule } from './pais/pais.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'pais', 
+    loadChildren: () => import('./pais/pais.module').then(m=>m.PaisModule)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
