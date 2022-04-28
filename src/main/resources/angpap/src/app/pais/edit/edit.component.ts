6d7c2d3b1bdfe302982c6eee21a9bb2c8995c2d7
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 <garay_alberto@hotmail.com>
2022-04-28 12:18:18 +0200

    kk

--- src/main/resources/angpap/src/app/pais/edit/edit.component.ts
+++ src/main/resources/angpap/src/app/pais/edit/edit.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { ActivatedRoute, Router } from '@angular/router';
-=======
 import { PaisService } from '../pais.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pais } from '../pais';
->>>>>>> 7a9bb6cea82952c26d1f9da30db7ff45fc322d11
 import { FormGroup, FormControl, Validators} from '@angular/forms';
-import { Pais } from '../pais';
-import { PaisService } from '../pais.service';
    
 @Component({
   selector: 'app-edit',
@@ -28,11 +22,7 @@ export class EditComponent implements OnInit {
   ) { }
   
   ngOnInit(): void {
-<<<<<<< HEAD
-    this.id = this.route.snapshot.params['postId'];
-=======
     this.id = this.route.snapshot.params['paisId'];
->>>>>>> 7a9bb6cea82952c26d1f9da30db7ff45fc322d11
     this.paisService.find(this.id).subscribe((data: Pais)=>{
       this.pais = data;
     });
@@ -50,13 +40,8 @@ export class EditComponent implements OnInit {
   submit(){
     console.log(this.form.value);
     this.paisService.update(this.id, this.form.value).subscribe(res => {
-<<<<<<< HEAD
-         console.log('Post updated successfully!');
-         this.router.navigateByUrl('post/index');
-=======
          console.log('País actualizado con éxito');
          this.router.navigateByUrl('pais/index');
->>>>>>> 7a9bb6cea82952c26d1f9da30db7ff45fc322d11
     })
   }
    
--- src/main/resources/angpap/src/app/pais/view/view.component.ts
+++ src/main/resources/angpap/src/app/pais/view/view.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { ActivatedRoute, Router } from '@angular/router';
-import { PaisService } from '../pais.service';
-=======
 import { PaisService } from '../pais.service';
 import { ActivatedRoute, Router } from '@angular/router';
->>>>>>> 7a9bb6cea82952c26d1f9da30db7ff45fc322d11
 import { Pais } from '../pais';
   
 @Component({
@@ -27,13 +22,8 @@ export class ViewComponent implements OnInit {
   ngOnInit(): void {
     this.id = this.route.snapshot.params['paisId'];
       
-<<<<<<< HEAD
-    this.paisService.find(this.id).subscribe((data)=>{
-      this.pais= data._embedded;
-=======
     this.paisService.find(this.id).subscribe((data: Pais)=>{
       this.pais = data;
->>>>>>> 7a9bb6cea82952c26d1f9da30db7ff45fc322d11
     });
   }
   
